2a0250176b0dccaf6ee44ac8a3e5a1fda45219e9
Prafull Prince <prafullprince12@gmail.com>
2025-01-03 03:21:57 +0530

    websocket

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,9 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from 'express-fileupload';
 
+// websocket
+import { WebSocket, WebSocketServer } from 'ws';
+
 // initialize port and app
 const app: Application = express();
 const PORT = process.env.PORT || 10000;
@@ -44,6 +47,43 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // port initialization
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+
+// websocket server
+const wss = new WebSocketServer({ server:httpServer });
+
+// userConnection
+export const userConnections = new Map<string, WebSocket>();
+
+// handle connection
+wss.on('connection',function connection(socket){
+    // error handle
+    socket.on('error',(err)=> console.log(err));
+
+    // message handle
+    socket.on('message', function message(data,isBinary){
+        try {
+            const userId = data.toString();
+            if(userId){
+                userConnections.set(userId,socket);
+                console.log('user connected')
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        // wss.clients.forEach(function each(client){
+        //     if(client.readyState === WebSocket.OPEN){
+        //         client.send(data,{binary: isBinary});
+        //     }
+        // })
+    })
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
+
+    // connection message
+    // socket.send('Hello : message from server!')
 });
